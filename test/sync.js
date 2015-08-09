314c562935b64132cbd3e6ced91adfd14f4cf22b
hex7c0 <carniellifrancesco@gmail.com>
2015-08-09 19:43:02 +0200

    test: fix xml sync

--- test/sync.js
+++ test/sync.js
@@ -2,7 +2,6 @@
 /**
  * @file sync test
  * @module dir2xml
- * @package dir2xml
  * @subpackage test
  * @version 0.0.1
  * @author hex7c0 <hex7c0@gmail.com>
@@ -12,41 +11,52 @@
 /*
  * initialize module
  */
-// import
-try {
-    var dir = require('../index.min.js'); // use require('dir2xml') instead
-    var assert = require('assert');
-} catch (MODULE_NOT_FOUND) {
-    console.error(MODULE_NOT_FOUND);
-    process.exit(1);
-}
+var dir = require('..');
+var assert = require('assert');
 
 /*
  * test module
  */
 describe('sync', function() {
 
-    var cache;
-    describe('json', function() {
+  var cache;
 
-        it('should return JSON object', function(done) {
+  describe('xml', function() {
 
-            cache = dir(__dirname, {
-                json: true
-            });
-            assert.deepEqual(cache[__dirname][0]['name'], 'exclude.js');
-            assert.deepEqual(cache[__dirname][1]['name'], 'sync.js');
-            done();
-        });
+    it('should return XML string', function(done) {
 
-        it('should return same object', function(done) {
-
-            var c = dir(__dirname, {
-                json: true
-            });
-            assert.deepEqual(cache, c);
-            done();
-        });
+      cache = dir(__dirname);
+      assert.notDeepEqual(cache, '');
+      done();
     });
 
+    it('should return same object', function(done) {
+
+      var c = dir(__dirname);
+      assert.deepEqual(cache, c);
+      done();
+    });
+  });
+
+  describe('json', function() {
+
+    it('should return JSON object', function(done) {
+
+      cache = dir(__dirname, {
+        json: true
+      });
+      assert.deepEqual(cache[__dirname][0]['name'], 'exclude.js');
+      assert.deepEqual(cache[__dirname][1]['name'], 'sync.js');
+      done();
+    });
+
+    it('should return same object', function(done) {
+
+      var c = dir(__dirname, {
+        json: true
+      });
+      assert.deepEqual(cache, c);
+      done();
+    });
+  });
 });
