58ea7662db2a3966e59d9117634e22a386c0c528
hex7c0 <carniellifrancesco@gmail.com>
2015-09-14 22:02:01 +0200

    test: check exclude cache/no-cache

--- test/exclude.js
+++ test/exclude.js
@@ -25,6 +25,38 @@ describe(
       'xml',
       function() {
 
+        describe(
+          'cache',
+          function() {
+
+            it('should return empty "__dirname" dir', function(done) {
+
+              var xml = dir(__dirname, {
+                exclude: /.js$/,
+                cache: true
+              });
+              assert.equal(xml,
+                '<?xml version="1.0" encoding="UTF-8"?><home></home>');
+              done();
+            });
+            it(
+              'should return 1 file into "__dirname" dir',
+              function(done) {
+
+                var xml = dir(__dirname, {
+                  exclude: /(exclude.js|dotfiles.js)$/,
+                  cache: true
+                });
+                assert
+                    .equal(
+                      /^<\?xml version="1.0" encoding="UTF-8"\?><home><file id="0"><name>sync.js<\/name><ctime>[^ ]+<\/hash><\/file><\/home>$/
+                          .test(xml), true);
+                assert.equal(/sync.js/.test(xml), true);
+                assert.equal(/exclude.js/.test(xml), false);
+                done();
+              });
+          });
+
         it('should return empty "__dirname" dir', function(done) {
 
           var xml = dir(__dirname, {
@@ -40,13 +72,13 @@ describe(
           function(done) {
 
             var xml = dir(__dirname, {
-              exclude: /exclude.js$/,
+              exclude: /(exclude.js|dotfiles.js)$/,
               cache: false
             });
             assert
-            .equal(
-              /^<\?xml version="1.0" encoding="UTF-8"\?><home><file id="0"><name>sync.js<\/name><ctime>[^ ]+<\/hash><\/file><\/home>$/
-              .test(xml), true);
+                .equal(
+                  /^<\?xml version="1.0" encoding="UTF-8"\?><home><file id="0"><name>sync.js<\/name><ctime>[^ ]+<\/hash><\/file><\/home>$/
+                      .test(xml), true);
             assert.equal(/sync.js/.test(xml), true);
             assert.equal(/exclude.js/.test(xml), false);
             done();
@@ -54,6 +86,32 @@ describe(
       });
 
     describe('json', function() {
+
+      describe('cache', function() {
+
+        it('should return empty "__dirname" dir', function(done) {
+
+          var json = JSON.stringify(dir(__dirname, {
+            exclude: /.js$/,
+            json: true,
+            cache: true
+          }));
+          assert.equal(json, '{}');
+          done();
+        });
+        it('should return 1 file into "__dirname" dir', function(done) {
+
+          var json = JSON.stringify(dir(__dirname, {
+            exclude: /(exclude.js|dotfiles.js)$/,
+            json: true,
+            cache: true
+          }));
+          assert.equal(/^{"[^ ]+","type":"file"}}}$/.test(json), true);
+          assert.equal(/sync.js/.test(json), true);
+          assert.equal(/exclude.js/.test(json), false);
+          done();
+        });
+      });
 
       it('should return empty "__dirname" dir', function(done) {
 
@@ -68,7 +126,7 @@ describe(
       it('should return 1 file into "__dirname" dir', function(done) {
 
         var json = JSON.stringify(dir(__dirname, {
-          exclude: /exclude.js$/,
+          exclude: /(exclude.js|dotfiles.js)$/,
           json: true,
           cache: false
         }));
